5185e2385bf0452c171a694db79665a001371b34
Olivia Ester <oliviaesterdev@gmail.com>
2019-06-10 13:34:28 -0700

    redux

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,15 +1,39 @@
 import { createStore } from 'redux'
 
-const store = createStore((state = {count: 0}) => {
-    if (action.type === 'INCREMENT') {
+const store = createStore((state = {count: 0}, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                count: state.count + 1
+            };
+        case 'DECREMENT':
+            return {
+                count: state.count - 1
+            }
+        case 'RESET':
+            return {
+                count: 0
+            }
+        default:
+        return state;
+    }
+});
+console.log(store.getState());
 
-    } else
-    console.log('running')
-    return state;
+store.dispatch({
+    type: 'INCREMENT'
 });
 
 store.dispatch({
     type: 'INCREMENT'
+});
+
+store.dispatch({
+    type: 'RESET'
+});
+
+store.dispatch({
+    type: 'DECREMENT'
 });
 
 
